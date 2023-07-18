7829cd93bc2bdd8a6eac57937f4e0a4e260b5082
Tuwan <ping.zhang@xuncetech.com>
2023-07-18 16:57:20 +0800

    fix: html插件

---
+++ src/index6.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './index6.module.less';
+
+let Greet = () => (<h1 style={style['box']}>Hello!</h1>);
+
+render(<Greet />, document.getElementById("root"));
+
